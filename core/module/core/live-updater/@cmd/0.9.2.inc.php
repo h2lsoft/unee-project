2dba81c4e60d52dbc7617028a22975060d01e7f9
H2LSOFT <contact@h2lsoft.com>
2024-11-19 00:05:06 +0100

    update query

--- core/module/core/live-updater/@cmd/0.9.2.inc.php
+++ core/module/core/live-updater/@cmd/0.9.2.inc.php
@@ -1,4 +1,4 @@
 <?php
 
-$sql ="INSERT INTO `xcore_widget` (`xcore_plugin_id`, `name`, `method`, `autorefresh`, `autorefresh_seconds`, `position`, `active`, `deleted`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES (24, 'Updater', ' \\Core\\Live_UpdaterController::widgetRender()', 'no', 0, 1, 'yes', 'no', '2024-05-28 16:25:13', 'superadmin', '2024-05-28 16:25:13', '', NULL, NULL)";
+$sql ="INSERT INTO `xcore_widget` (`xcore_plugin_id`, `name`, `method`, `autorefresh`, `autorefresh_seconds`, `position`, `active`, `deleted`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES (24, 'Updater', '\\Core\\Live_UpdaterController::widgetRender()', 'no', 0, 1, 'yes', 'no', '2024-05-28 16:25:13', 'superadmin', '2024-05-28 16:25:13', '', NULL, NULL)";
 DB()->query($sql);
