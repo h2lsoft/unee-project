437aa051484b3347ab1eb7bc82b84cbd23f51ea0
H2LSOFT <contact@h2lsoft.com>
2024-12-17 11:57:01 +0100

    update

--- core/command/app/install.sql
+++ core/command/app/install.sql
@@ -798,6 +798,71 @@ INSERT INTO `xcore_widget` (`id`, `xcore_plugin_id`, `name`, `method`, `autorefr
 
 INSERT INTO `xcore_page_zone` (`id`, `name`, `website`, `position`, `deleted`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES (1, 'MAIN MENU', '', 1, 'no', NOW(), 'superadmin', NULL, NULL, NULL, NULL);
 
-INSERT INTO `xcore_crontask` (`id`, `category`, `name`, `description`, `script_path`, `script_parameters`, `repetition_months`, `repetition_days`, `repetition_hours`, `repetition_minutes`, `email_report`, `email_report_subject`, `email_report_recipients`, `email_report_failure`, `status`, `locked`, `locked_date`, `last_error_message`, `last_execution_date_start`, `last_execution_date_end`, `priority`, `active`, `deleted`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES (1, 'Core', 'Reset page and article status', '', '/core/cron/system/page.inc.php', '', '*', '*', '*', '0,5,10,15,20,25,30,35,40,45,50,55', 'no', '', '', 'no', 'finish', 'no', '2024-11-17 12:56:02', '', '2024-11-17 12:56:02', '2024-11-17 12:56:02', 0, 'yes', 'no', '2024-11-17 12:26:23', 'superadmin', '2024-11-17 12:56:02', '', NULL, NULL)
+INSERT INTO `xcore_crontask` VALUES (1, 'System', 'Cleaner', '', '/core/cron/maintenance.inc.php', '', '*', '*', '4', '0', 'no', '', '', 'no', '', 'no', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'yes', 'no', '2024-07-04 09:05:59', 'superadmin', '2024-07-04 09:10:27', 'superadmin', NULL, NULL);
+INSERT INTO `xcore_crontask` VALUES (2, 'System', 'Page and article', '', '/core/cron/system/page.inc.php', '', '*', '*', '*', '0,10,20,30,40,50', 'no', '', '', 'no', '', 'no', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 'yes', 'no', '2024-11-17 20:17:19', 'superadmin', '2024-12-16 22:49:01', 'superadmin', NULL, NULL);
+INSERT INTO `xcore_crontask` VALUES (3, 'System', 'Newsletter', '', '/core/cron/newsletter.inc.php', '', '*', '*', '*', '0', 'no', '', '', 'no', '', 'no', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 'yes', 'no', '2024-12-16 22:47:20', 'superadmin', NULL, NULL, NULL, NULL);
 
 
+
+
+CREATE TABLE `xcore_newsletter` (
+	`id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`date` DATETIME NOT NULL,
+	`category` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`campaign` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`expeditor_email` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`expeditor_email_label` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`reply_to` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`subject` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`template_url` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`link_add_parameters` TEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+	`mailinglist_ids` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`status` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`programming_date` DATETIME NOT NULL,
+	`scheduler_date_start` DATETIME NOT NULL,
+	`scheduler_date_end` DATETIME NOT NULL,
+	`scheduler_report_email` ENUM('yes','no') NOT NULL DEFAULT 'no' COLLATE 'utf8mb4_general_ci',
+	`scheduler_report_email_recipients` TEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+	`deleted` ENUM('yes','no') NOT NULL DEFAULT 'no' COLLATE 'utf8mb4_general_ci',
+	`created_at` DATETIME NULL DEFAULT NULL,
+	`created_by` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`updated_at` DATETIME NULL DEFAULT NULL,
+	`updated_by` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`deleted_at` DATETIME NULL DEFAULT NULL,
+	`deleted_by` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE,
+	INDEX `date` (`date`) USING BTREE,
+	INDEX `status` (`status`(250)) USING BTREE,
+	INDEX `deleted` (`deleted`) USING BTREE,
+	INDEX `category` (`category`(250)) USING BTREE,
+	INDEX `programming_date` (`programming_date`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=MyISAM;
+
+
+CREATE TABLE `xcore_newsletter_data` (
+	`id` BIGINT(20) UNSIGNED NOT NULL,
+	`xcore_newsletter_id` BIGINT(20) UNSIGNED NOT NULL,
+	`xcore_mailinglist_id` BIGINT(20) UNSIGNED NOT NULL,
+	`xcore_mailinglist_subscriber_id` BIGINT(20) UNSIGNED NOT NULL,
+	`date` DATETIME NOT NULL,
+	`action` ENUM('sent','unsubscribe','view','click') NOT NULL COLLATE 'utf8mb4_general_ci',
+	`url` TEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+	`deleted` ENUM('yes','no') NOT NULL DEFAULT 'no' COLLATE 'utf8mb4_general_ci',
+	`created_at` DATETIME NULL DEFAULT NULL,
+	`created_by` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`updated_at` DATETIME NULL DEFAULT NULL,
+	`updated_by` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`deleted_at` DATETIME NULL DEFAULT NULL,
+	`deleted_by` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE,
+	INDEX `xcore_mailinglist_id` (`xcore_mailinglist_id`) USING BTREE,
+	INDEX `xcore_mailinglist_subscriber_id` (`xcore_mailinglist_subscriber_id`) USING BTREE,
+	INDEX `date` (`date`) USING BTREE,
+	INDEX `action` (`action`) USING BTREE,
+	INDEX `xcore_newsletter_id` (`xcore_newsletter_id`) USING BTREE,
+	INDEX `deleted` (`deleted`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=MyISAM;
