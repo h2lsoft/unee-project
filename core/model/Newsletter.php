3e206258ea26e2a6719131563ee3ae7ffc5e999d
H2LSOFT <contact@h2lsoft.com>
2024-12-17 11:45:39 +0100

    update

---
+++ core/model/Newsletter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace model;
+
+class Newsletter extends \Core\Entity
+{
+	public static string $table = 'xcore_newsletter';
+
+
+	public static function send(string|array $from, string $to, string $subject, string $body, array $data=[], array $options=[], array $headers=[], array $json_added=[])
+	{
+		// parse links
+		$pattern = '/<a\s+[^>]*href=["\']([^"\']+)["\']/i';
+		if(preg_match_all($pattern, $body, $matches))
+		{
+			$links = $matches[1];
+			foreach($links as $link)
+			{
+				$json = $json_added;
+				$json['url'] = $link;
+				$link2 = \Core\Config::get('url')."/service/newsletter/link/?uri=".urlencode(base64_encode(json_encode($json)));
+				$body = str_replace($link, $link2, $body);
+			}
+		}
+
+		\Core\Mailer::send($from, $to, $subject, $body, [], $options, $data, $headers);
+	}
+
+
+}
