afcc41ca49542dbeb6ebe574d074b8a8dc676f56
H2LSOFT <contact@h2lsoft.com>
2024-12-28 22:21:32 +0100

    update 0.9.9

--- core/model/Live_Updater.php
+++ core/model/Live_Updater.php
@@ -4,7 +4,7 @@ namespace Model;
 
 class Live_Updater extends \Core\Entity
 {
-	public static string $version = '0.9.7'; # do not edit this line
+	public static string $version = '0.9.9'; # do not edit this line
 
 
 	/**
