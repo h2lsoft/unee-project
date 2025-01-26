b50c6bf723270569d175c4d30c52582ec3cbb228
H2LSOFT <contact@h2lsoft.com>
2025-01-26 14:52:22 +0100

    add auto set

--- core/lib/class/Globals.php
+++ core/lib/class/Globals.php
@@ -5,7 +5,7 @@ namespace Core;
 class Globals
 {
 	static public array $cached = [];
-	
+
 	/**
 	 * get global value from variable name
 	 *
@@ -14,16 +14,21 @@ class Globals
 	 *
 	 * @return string|bool
 	 */
-	public static function get(string $name, mixed $default=false):bool|string
+	public static function get(string $name, mixed $default=false, bool $register_value=false):bool|string
 	{
 		global $app;
-		
+
+		// register
+		if($register_value && !isset($app->globals[$name]))
+		{
+			$app->globals[$name] = $default;
+			\Core\Globals::set($name, $default);
+		}
+
 		$value = (!isset($app->globals[$name])) ? $default : $app->globals[$name];
-		
 		return $value;
-		
 	}
-	
+
 	/**
 	 * create or update a globals
 	 *
@@ -35,24 +40,24 @@ class Globals
 	public static function set(string $name, string $value, string $package=''):void
 	{
 		if(empty($package))$package = APP_PACKAGE;
-		
+
 		\Core\Entity::$table = 'xcore_globals';
-		
-		
+
+
 		$f = [];
 		$f['package'] = $package;
 		$f['name'] = $name;
 		$f['value'] = $value;
-		
+
 		$where = [];
 		$where[] = "package = :package and name = :name";
 		$where[] = [':package' => $package, ':name' => $name];
-		
-		
+
+
 		\Core\Entity::replace($f, $where);
-		
+
 	}
-	
+
 	/**
 	 * @param string $name
 	 * @param string $package if empty current package
@@ -62,21 +67,21 @@ class Globals
 	public static function destroy(string $name, string $package=''):void
 	{
 		global $app;
-		
+
 		if(empty($package))$package = APP_PACKAGE;
-		
+
 		\Core\Entity::$table = 'xcore_globals';
-		
+
 		$where = [];
 		$where[] = "package = :package and name = :name";
 		$where[] = [':package' => $package, ':name' => $name];
-		
+
 		\Core\Entity::delete($where);
-		
+
 		if(isset($app->globals[$name]))
 			unset($app->globals[$name]);
 	}
-	
-	
-	
+
+
+
 }
